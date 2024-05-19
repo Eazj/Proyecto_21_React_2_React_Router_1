bbd557bfa3fc7a6f3364cb15504ee16c00c69ca9
Eleazar Jara <eleazartmw15@gmail.com>
2024-05-18 21:11:13 -0400

    se crea proyecto y se realiza limpieza

---
+++ react_router_1/src/components/Link.jsx
@@ -0,0 +1,8 @@
+function Link(){
+    return(
+        <>
+        <h1>Soy el Componente Link</h1>
+        </>
+    )
+}
+export default Link;
---
+++ react_router_1/src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+function Navbar(){
+    return(
+        <>
+        <h1>Soy el Componente Navbar</h1>
+        </>
+    )
+}
+export default Navbar;
